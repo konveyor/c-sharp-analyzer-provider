9b12c8850033901f351242ec20be6b5ef6bbd599
Savitha Raghunathan <saveetha13@gmail.com>
2025-12-11 17:08:31 -0500

    update dep and protoc path
    Signed-off-by: Savitha Raghunathan <saveetha13@gmail.com>

--- build.rs
+++ build.rs
@@ -21,7 +21,7 @@ fn main() {
 
         tonic_build::configure()
             .file_descriptor_set_path("src/analyzer_service/provider_service_descriptor.bin")
-            .compile_protos(&["src/build/proto/provider.proto"], &["proto"])
+            .compile_protos(&["src/build/proto/provider.proto"], &["src/build/proto/"])
             .unwrap();
     }
 
